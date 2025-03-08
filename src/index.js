659b23e1c3f88f234ecfe90d5c2323bcde6f4c12
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-03-08 14:53:41 +0500

    asd

--- src/index.js
+++ src/index.js
@@ -3,48 +3,49 @@ const cors = require("cors");
 const PORT = 5000;
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
-const users = [];
+const users = [
+  { id: 1, firstName: "Doni", lastName: "Qochqorova", phone: "1234567890", password: "password123", email: ""},
+];
+
+app.post("/api/v1/login", (req, res) => {
+  const { phone, password } = req.body; 
+  const user = users.find((user) => user.phone === phone && user.password === password);
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  res.status(200).json({
+    message: "Login successful",
+    user: { id: user.id, firstName: user.firstName, lastName: user.lastName, phone: user.phone },
+  });
+});
+
+app.post("/api/v1/register", (req, res) => {
+  const { firstName, lastName, password, email } = req.body;
+  if (!firstName || !lastName || !password || !email) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
+
+  const existingUser = users.find((user) => user.email === email);
+
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exists" });
+  }
+
+  res.status(201).json({
+    message: "User registered successfully",
+    user: { firstName, lastName, password, email },
+  });
+});
 
 app.get("/api/v1/users", (req, res) => {
   res.json(users);
 });
 
-app.post("/api/v1/login", (req, res) => {
-  res.json(users);
-});
-
-app.post("/api/v1/register", (req, res) => {
-  const { username, password, phone } = req.body;
-
-  if (!username || !password || !phone) {
-    return res.status(400).json({ message: "Please provide all fields" });
-  }
-
-  const existingUser = users.find(
-    (user) => user.username === username || user.phone === phone
-  );
-
-  if (existingUser) {
-    return res
-      .status(400)
-      .json({ message: "Username or phone already exists" });
-  }
-
-  res.status(201).json({
-    message: "User registered successfully",
-    user: { username, password, phone },
-  });
-});
-
 app.listen(PORT, () => {
-  console.log(
-    `================================================================`
-  );
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Server created by Bekzod Mirzaaliyev`);
-  console.log(
-    `================================================================`
-  );
 });
