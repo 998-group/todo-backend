327246b56ab0969ec6fd49361448e34d98937891
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-03-20 15:39:45 +0500

    ASD

--- src/model/StoryModel.js
+++ src/model/StoryModel.js
@@ -1,15 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userStorySchema = new mongoose.Schema({
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    comments: { type: Number, default: 0 },
-    like: { type: Number, default: 0 },
-    publicationDate: { type: Date, default: Date.now },
-    status: { type: String, default: 'active', enum: ["active", "inactive"] },
-    stories: [{
-        thumbnail: { type: String, require: false},
-        video: { type: String, require: false}
-    }]
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  comments: { type: Number, default: 0 },
+  like: { type: Number, default: 0 },
+  publicationDate: { type: Date, default: Date.now },
+  status: { type: String, default: "active", enum: ["active", "inactive"] },
+  stories: [
+    {
+      thumbnail: { type: String, require: false },
+      video: { type: String, require: false },
+    },
+  ],
 });
 
-module.exports = mongoose.model('UserStory', userStorySchema);
+// Автоматическая проверка перед запросами (middleware)
+userStorySchema.pre("find", function (next) {
+  const now = new Date();
+  this.updateMany(
+    { publicationDate: { $lte: new Date(now - 24 * 60 * 60 * 1000) } },
+    { status: "inactive" }
+  ).exec();
+  next();
+});
+
+module.exports = mongoose.model("UserStory", userStorySchema);
