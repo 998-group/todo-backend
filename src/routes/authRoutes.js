47665f4da2c75f1377ddbf401b21fbbb13460e95
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-04-15 15:29:45 +0500

    ASD

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -61,4 +61,13 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find().select("_id name email");
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users" });
+  }
+});
+
 module.exports = router;
